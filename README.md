69690e581827297e2592a5ac705e75de4c24b6e2
Stuart <stuiek1985@gmail.com>
2025-04-02 16:34:50 +0100

    added setting and more readme

--- README.md
+++ README.md
@@ -1,3 +1,27 @@
+# Custom home page for your browser
+
+This has both standard HTML, CSS, JavaScript and React versions.
+Both at this point are not fully working
+
+Not yet build or working
+
+- [ ] settings page
+  - [ ] nice animation slides in from side with hamburger setting icon
+  - [ ] Custom name display box (toggle to disable for added customisability)
+  - [ ] Welcome background image (toggle to disable for added customisability)
+  - [ ] Smaller/profile image (toggle to disable for added customisability)
+  - [ ] Openweather api key (toggle to disable all weather for added customisability)
+  - [ ] Postcode region for fetching weather location
+  - [ ] Add new folder / tag (some how let user add icons or images)
+  - [ ] add new bookmark with a dropdown box of the folders
+  - [ ] add in delete button somehow
+  - [ ] reset all button
+- [ ] reusable folder/tag for bookmarks
+- [ ] auto gen for the flavicon beside bookmark link (test with full link for icons)
+- [ ] change 3 day weather to re-align ( looks messy )
+- [ ] light and Dark mode
+- [ ] wallpaper/ image in welcome box.
+
 # React + Vite
 
 This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
--- customStartpage/script.js
+++ customStartpage/script.js
@@ -1,3 +1,51 @@
+// ############################# Onload Functions ########################
+function start() {
+  startTime();
+  greet();
+  getWeather();
+  dark();
+}
+// ############################# Settings ########################
+
+const darkMode = document.getElementById("darkMode");
+
+function dark() {
+  if (darkMode.checked) {
+    /* dark */
+    document.querySelector(":root").style.setProperty("--text", "#dddddd");
+    document.querySelector(":root").style.setProperty("--bg", "#21232b");
+  } else if (!darkMode.checked) {
+    /*  light */
+    document.querySelector(":root").style.setProperty("--text", "#00000");
+    document.querySelector(":root").style.setProperty("--bg", "#dddddd");
+  }
+  return;
+}
+
+function Welcome() {
+  if (showWelcome.checked) {
+    /* dark */
+    document.querySelector(":root").style.setProperty("--text", "#dddddd");
+    document.querySelector(":root").style.setProperty("--bg", "#21232b");
+  } else if (!darkMode.checked) {
+    /*  light */
+    document.querySelector(":root").style.setProperty("--text", "#00000");
+    document.querySelector(":root").style.setProperty("--bg", "#dddddd");
+  }
+  return;
+}
+
+const showWeather = document.getElementById("showWeather");
+function Weather() {
+  if (showWeather.checked) {
+    document.querySelector("#weather").style.setProperty("display", "flex");
+  } else if (!showWeather.checked) {
+    document.querySelector("#weather").style.setProperty("display", "none");
+  }
+}
+
+// ############################# CLOCK ########################
+
 // future set up for toggle 12 and 24 hour
 function startTime() {
   const today = new Date();
@@ -33,7 +81,8 @@ function checkTime(i) {
   return i;
 }
 
-function welcome() {
+// ############################# Greeting ########################
+function greet() {
   if (new Date().getHours() >= 13) {
     timeOfDay = "Afternoon";
   }
@@ -48,6 +97,8 @@ function welcome() {
 
   document.getElementById("text").innerHTML = `Good ${timeOfDay}, Stuart`;
 }
+
+// ############################# search bar ########################
 
 // works with enter too sshhh not sure how but its a feature!!
 document
@@ -71,28 +122,140 @@ Remove All (Clear Local Storage)
 localStorage.clear();
 */
 
-function start() {
-  startTime();
-  welcome();
+/*
+    const apiKey = "827cd00c74aa212544f43a7c47b3922a"; // Replace with your OpenWeather API key
+    const city = "London";
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+    fetch(url)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then((data) => {
+        console.log("Weather Data:", data);
+      })
+      .catch((error) => {
+        console.error("Error fetching weather data:", error);
+      });
+    //https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
+
+    //https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
+*/
+
+// ############################# Weather API ########################
+
+function getLocation() {
+  const apiKey = "f7885e7e86b598f6edc06a41fbaaa6ab"; // Replace with your OpenWeather API key
+  const zipCode = "BT38";
+  const countryCode = "GB";
+  const url = `http://api.openweathermap.org/geo/1.0/zip?zip=${zipCode},${countryCode}&appid=${apiKey}`;
+  fetch(url)
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      localStorage.setItem("lat", data.lat);
+      localStorage.setItem("lon", data.lon);
+      //console.log(lat, lon); // make this save as local storage
+    });
 }
 
-const apiKey = "827cd00c74aa212544f43a7c47b3922a"; // Replace with your OpenWeather API key
-const city = "London";
-const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+function getWeather() {
+  getLocation();
+  const apiKey = "f7885e7e86b598f6edc06a41fbaaa6ab";
+  const lat = localStorage.getItem("lat");
+  const lon = localStorage.getItem("lon");
 
-fetch(url)
-  .then((response) => {
-    if (!response.ok) {
-      throw new Error(`HTTP error! Status: ${response.status}`);
-    }
-    return response.json();
-  })
-  .then((data) => {
-    console.log("Weather Data:", data);
-  })
-  .catch((error) => {
-    console.error("Error fetching weather data:", error);
-  });
-//https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
+  //const url = `https://api.openweathermap.org/data/3.0/onecall?lat=54.728&lon=-5.756&exclude=hourly,minutely&appid=${apiKey}&units=metric`;
 
-//https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
+  const url = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&exclude=hourly,minutely&appid=${apiKey}&units=metric`;
+
+  fetch(url)
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      const currTemp = document.getElementById("currTemp");
+      const currImg = document.getElementById("currImg");
+      const currDescription = document.getElementById("currDescription");
+      const days = document.getElementById("weatherDays");
+      const daysImg = document.getElementById("daysImg");
+      const daysDescription = document.getElementById("daysDescription");
+
+      // DAYS
+      const today = new Date();
+      const weekday = [
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+        "Sunday",
+      ];
+
+      // Today
+      const descript = data.current.weather[0].description;
+      const temp = data.current.temp;
+      const icon = data.current.weather[0].icon;
+      const currMin = data.daily[0].temp.min;
+      const currMax = data.daily[0].temp.max;
+
+      currTemp.innerHTML = `${Math.round(temp)}°`;
+      currImg.innerHTML = `<img src="https://openweathermap.org/img/wn/${icon}@2x.png"></img>`;
+      currDescription.innerHTML = `${descript} / High ${Math.round(
+        currMax
+      )}°  Low ${Math.round(currMin)}°`;
+
+      // Day 1
+      const day1Name =
+        `${today.getDay() + 1}` > 6
+          ? `${weekday[0]}`
+          : `${weekday[today.getDay() + 1]}`;
+      const day1ImgData = data.daily[1].weather[0].icon;
+      const day1Img = `<img src="https://openweathermap.org/img/wn/${day1ImgData}.png"></img>`;
+      const day1Descrit = data.daily[1].weather[0].description;
+
+      // Day 2
+      const day2Name =
+        `${today.getDay() + 1}` > 6
+          ? `${weekday[0]}`
+          : `${weekday[today.getDay() + 1]}`;
+      const day2ImgData = data.daily[2].weather[0].icon;
+      const day2Img = `<img src="https://openweathermap.org/img/wn/${day2ImgData}.png"></img>`;
+      const day2Descrit = data.daily[2].weather[0].description;
+
+      // Day 3
+      const day3Name =
+        `${today.getDay() + 2}` > 6
+          ? `${weekday[0]}`
+          : `${weekday[today.getDay() + 2]}`;
+      const day3ImgData = data.daily[3].weather[0].icon;
+      const day3Img = `<img src="https://openweathermap.org/img/wn/${day3ImgData}.png"></img>`;
+      const day3Descrit = data.daily[3].weather[0].description;
+
+      days.innerHTML = `<div id=day1> ${day1Name} </div><div id=day1Img>${day1Img}</div><div id=day1> ${day1Descrit} </div>`;
+      daysImg.innerHTML = `<div id=day2> ${day2Name} </div><div id=day2Img>${day2Img}</div><div id=day2> ${day2Descrit} </div>`;
+      daysDescription.innerHTML = `<div id=day3> ${day3Name} </div><div id=day3Img>${day3Img}</div><div id=day3> ${day3Descrit} </div>`;
+    });
+}
+
+//   Calling weather 4 times a day i needed.
+
+function newWeather() {
+  if (new Date().getHours() === 6) {
+    getWeather();
+  }
+  if (new Date().getHours() === 12) {
+    getWeather();
+  }
+  if (new Date().getHours() === 15) {
+    getWeather();
+  }
+  if (new Date().getHours() === 5) {
+    getWeather();
+  }
+}
